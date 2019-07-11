810ae1eb671e9a28713568af5a7c4b8efef32c6b
Shivani123789 <shivanipawar1995@gmail.com>
2019-07-11 15:56:56 +0530

    payment page added

--- manoranjan/src/app/app.module.ts
+++ manoranjan/src/app/app.module.ts
@@ -68,6 +68,7 @@ import { LinkMovieNorComponent } from './link-movie-nor/link-movie-nor.component
 import { LinkMoviePriComponent } from './link-movie-pri/link-movie-pri.component';
 import { LinkWebNorComponent } from './link-web-nor/link-web-nor.component';
 import { LinkWebPriComponent } from './link-web-pri/link-web-pri.component';
+import { PaymentComponent } from './payment/payment.component';
 
 
 @NgModule({
@@ -124,7 +125,8 @@ import { LinkWebPriComponent } from './link-web-pri/link-web-pri.component';
     LinkMovieNorComponent,
     LinkMoviePriComponent,
     LinkWebNorComponent,
-    LinkWebPriComponent
+    LinkWebPriComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
