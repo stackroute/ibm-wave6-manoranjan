5f61653263bb6295e1a3b30b7bc502ab212224c3
anusha <vanushabhavanasi@gmail.com>
2019-07-16 14:52:25 +0530

    producer profile

---
+++ manoranjan/src/app/uploadedvideos/uploadedvideos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uploadedvideos',
+  templateUrl: './uploadedvideos.component.html',
+  styleUrls: ['./uploadedvideos.component.css']
+})
+export class UploadedvideosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
