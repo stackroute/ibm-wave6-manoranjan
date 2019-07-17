ebbdd401c12fd4efb21a3c470c1e59f415cbd761
madhurih <hiremadhuri66@gmail.com>
2019-07-17 10:33:54 +0530

    carosel and slider images effects changed

--- manoranjan/src/app/firstcard/firstcard.component.ts
+++ manoranjan/src/app/firstcard/firstcard.component.ts
@@ -11,20 +11,20 @@ export class FirstcardComponent implements OnInit {
 
   constructor() {
     this.items=[
-      {name:'../../assets/mov1.webp'},
-      {name:'../../assets/mov2.webp'},
+      {name:'../../assets/caro/5.jpg'},
       {name:'../../assets/mov3.webp'},
-      {name:'../../assets/mov4.webp'},
+      {name:'../../assets/caro/2.jpg'},
+      {name:'../../assets/caro/1.jpg'},
       {name:'../../assets/mov5.webp'},
       {name:'../../assets/mov1.webp'},
-      {name:'../../assets/mov2.webp'},
-      {name:'../../assets/ddlj.jpeg'},
+      {name:'../../assets/caro/4.jpg'},
+      {name:'../../assets/caro/3.jpg'},
       {name:'../../assets/dch.jpeg'},
       {name:'../../assets/kahani.jpeg'},
-      {name:'../../assets/lagan.jpeg'},
+      {name:'../../assets/mov4.webp'},
       {name:'../../assets/three idiots.jpeg'},
-      {name:'../../assets/dch.jpeg'},
-      {name:'../../assets/kahani.jpeg'},
+      {name:'../../assets/m1.jpeg'},
+      {name:'../../assets/lagan.jpeg'},
       {name:'../../assets/ddlj.jpeg'},
     ];
    }
