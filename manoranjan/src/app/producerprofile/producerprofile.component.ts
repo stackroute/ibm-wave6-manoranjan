7c63516506f214d0eb890fd90882a0f2f2e2d9bd
pooja <poojaspatil13293@gmail.com>
2019-07-30 16:38:21 +0530

    changes in name

--- manoranjan/src/app/producerprofile/producerprofile.component.ts
+++ manoranjan/src/app/producerprofile/producerprofile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-producerprofile',
@@ -11,11 +12,18 @@ export class ProducerprofileComponent implements OnInit {
   users:User;
   user=new User();
   emailId;
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.user.emailId=sessionStorage.getItem('email');
-      console.log(this.user.emailId)
+      console.log(this.user.emailId);
+      this.userService.getById(this.user.emailId).subscribe(data => {
+        this.users = data;
+        console.log("POST Request is successful ", data);},
+        error => {
+          // alert("Login Unsuccessful, tryagain")
+          console.log("Error", error);}
+      );
   }
 
 }
