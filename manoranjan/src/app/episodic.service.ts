4da2763644d4b805f611103e36d34d4b872237c0
madhurih <hiremadhuri66@gmail.com>
2019-08-05 17:32:33 +0530

    changed address

--- manoranjan/src/app/episodic.service.ts
+++ manoranjan/src/app/episodic.service.ts
@@ -10,8 +10,8 @@ export class EpisodicService {
   constructor(private http: HttpClient) { }
 
   getShowsByLanguage(language:string){
-//     return this.http.get('http://13.235.52.81:8083/episodic-media-service/series/tv/'+language,{
-      return this.http.get('http://localhost:8083/episodic-media-service/series/tv/'+language,{
+    return this.http.get('http://13.235.52.81:8083/episodic-media-service/series/tv/'+language,{
+      // return this.http.get('http://localhost:8083/episodic-media-service/series/tv/'+language,{
         headers:new HttpHeaders({
         'Access-Control-Allow-Origin' : '*'
       })
@@ -19,20 +19,20 @@ export class EpisodicService {
   }
 
   getEpisodic(series:string){
-//     return this.http.get('http://13.235.52.81:8083/episodic-media-service/series/category/'+series,{
-      return this.http.get('http://localhost:8083/episodic-media-service/series/category/'+series,{
+    return this.http.get('http://13.235.52.81:8083/episodic-media-service/series/category/'+series,{
+      // return this.http.get('http://localhost:8083/episodic-media-service/series/category/'+series,{
         headers:new HttpHeaders({
         'Access-Control-Allow-Origin' : '*'
       })
     });
   }
   saveSerial(serial:Episodic){
-//     return this.http.post<Episodic>("http://13.235.52.81:8083/episodic-media-service/serial",serial);
-    return this.http.post<Episodic>("http://localhost:8083/episodic-media-service/serial",serial);
+    return this.http.post<Episodic>("http://13.235.52.81:8083/episodic-media-service/serial",serial);
+    // return this.http.post<Episodic>("http://localhost:8083/episodic-media-service/serial",serial);
   }
 
   getWishlist(titles:Array<string>){
-//     return this.http.patch('http://13.235.52.81:8083/episodic-media-service/wish',titles);
-    return this.http.patch('http://localhost:8083/episodic-media-service/wish',titles);
+    return this.http.patch('http://13.235.52.81:8083/episodic-media-service/wish',titles);
+    // return this.http.patch('http://localhost:8083/episodic-media-service/wish',titles);
   }
 }
